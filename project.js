a576a74831b4b3616c8709c9f67ac1807245e0a9
Hazem shaban <hs3833024@gmail.com>
2025-02-19 03:51:38 +0200

    navbar

--- project.js
+++ project.js
@@ -1,3 +1,10 @@
+let head = document.querySelector("header") ;
+window.addEventListener("scroll" , function(){
+    head.classList.toggle("change" , this.window.scrollY > 0 );
+})
+
+
+
 let btn = document.getElementById("btn") ;
 btn.onclick = function() {
     scroll({top : 0 , behavior:"smooth"})
